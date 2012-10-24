84f7681e8b9cc4dd08e236dfd33eb2e43bea3be3
dan branton <dtotheb@gmail.com>
2012-10-23 20:07:18 -0400

    Did some more Letters & setup a README

--- 8x8DAN.py
+++ 8x8DAN.py
@@ -7,16 +7,22 @@ import LetterArrays
 
 grid = EightByEight(address=0x70)
 
-print "Press CTRL+Z to exit"
+def drawFrame(frame, grid, dur=0):
+    for pix in frame:
+        grid.setPixel(pix[0],pix[1])
+        time.sleep(dur)
 
+
+word = LetterArrays.wordForOutput
 #loop through the letters
 while (True):
-    for letter in LetterArrays.wordForOutput:
-        c = LetterArrays.letterDict[letter]
-        for p in c:
-            grid.setPixel(p[0], p[1])
-            time.sleep(0.005)
-        time.sleep(0.5)
-        grid.clear()
+    for letter in word:
+        if letter in LetterArrays.CHARS:
+            c = LetterArrays.CHARS[letter]
+            c = LetterArrays.CHARS[letter]
+            drawFrame(c, grid)
+            time.sleep(0.5)
+            grid.clear()
 
     reload(LetterArrays)
+    word = LetterArrays.wordForOutput
